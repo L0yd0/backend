ff39f34ad8c29feb361f35c245a6e2041f91e73f
Ricardo Vargas <ricardovargas120717@gmail.com>
2025-11-01 13:36:17 -0600

    nuevo

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ connectDB()
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/tareas', require('./routes/tareasRoutes'));
